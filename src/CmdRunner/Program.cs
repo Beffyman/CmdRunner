3ff25646bdb77ec0bdbe331407db4b3d8b933b3d
Connor Laderer <splashy5@hotmail.com>
2018-01-01 18:56:45 -0500

    initial commit

---
+++ src/CmdRunner/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using ElectronNET.API;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace CmdRunner
+{
+	public class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			await WebHost.CreateDefaultBuilder(args)
+				//.UseElectron(args)
+				.UseStartup<Startup>()
+				.Build()
+				.RunAsync();
+		}
+
+	}
+}
